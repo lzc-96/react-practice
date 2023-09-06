5bd64e5b09094025216f2fb92a3b39d9bb045604
Zhi Cheng <zhicheng135@hotmail.com>
2023-09-07 00:54:31 +0800

    test commit 2

--- code/04-outputting-conditional-content/src/components/Expenses/ExpenseDate.js
+++ code/04-outputting-conditional-content/src/components/Expenses/ExpenseDate.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
+  console.log("test");
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
